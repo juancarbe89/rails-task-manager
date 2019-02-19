6b789b03f49179059b58cf3ec9f4783acde5b800
Juan Cardenas <juancarbe89@gmail.com>
2019-02-20 10:34:40 +1100

    controller and views created

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :title, :details, presence: true
+  # validates :details, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read all Tasks
+  get 'tasks', to: 'tasks#index'
+
+  # Create one Task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # Read one Task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # Update one Task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'Tasks/:id', to: 'tasks#update'
+
+  # Delete one Task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
